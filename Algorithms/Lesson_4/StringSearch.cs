108795628f874cf8ade60d15a7c9b4142eb05e6b
Evgeniy <eugene-1997@inbox.ru>
2021-07-14 19:43:13 +0600

    add Lesson_4.Task_1

---
+++ Algorithms/Lesson_4/StringSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Lesson_4
+{
+    static class StringSearch
+    {
+        public static bool Search(string item, params string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == item) return true;
+
+            return false;
+        }
+        public static bool Search(string item, HashSet<string> items)
+        {
+            return items.Contains(item);
+        }
+    }
+}
---
+++ Algorithms/Lesson_4/StringSearchBenchmarks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Algorithms.Lesson_4
+{
+    public class StringSearchBenchmarks
+    {
+        private static string[] _array;
+        private static HashSet<string> _hashSet;
+
+        static StringSearchBenchmarks()
+        {
+            _array = new string[10000];
+            for (int i = 0; i < _array.Length; i++) _array[i] = GeneratString(10);
+            _hashSet = new HashSet<string>(_array);
+        }
+        [Benchmark]
+        public void LinearSearchString()
+        {
+            StringSearch.Search(_array[new Random().Next(_array.Length)], _array);
+        }
+        [Benchmark]
+        public void SearchStringByHashSet()
+        {
+            StringSearch.Search(_array[new Random().Next(_array.Length)], _hashSet);
+        }
+        private static string GeneratString(int wordSize)
+        {
+            Random rnd = new Random();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < wordSize; i++)
+                result.Insert(i, rnd.Next('a', 'z' + 1));
+
+            return result.ToString();
+        }
+    }
+}
