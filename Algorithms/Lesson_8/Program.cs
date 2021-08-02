b48ae6f9aefd6f5b4f99f99c64b0b24df56f3295
EvgeniyRuban <84354312+EvgeniyRuban@users.noreply.github.com>
2021-08-02 21:46:35 +0500

    Add Lesson_8

---
+++ Algorithms/Lesson_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algorithms.Lesson_8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[30];
+            Random rnd = new Random();
+            for(int i=0; i <array.Length; i++) array[i] = rnd.Next(100);
+            int[] array_1 = (int[])array.Clone();
+            int[] array_2 = (int[])array.Clone();
+
+            Console.WriteLine("Array generated:");
+            Print(array);
+
+            Console.WriteLine("\nBucketsort:");
+            Print(Sorter.Bucketsort(array_1));
+
+            Console.WriteLine("\nQuicksort:");
+            Print(Sorter.Quicksort(array_2, 0, array_2.Length - 1));
+
+            Console.ReadKey();
+        }
+        public static void Print(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(i == 0) Console.Write($" {array[i]}");
+                else Console.Write($", {array[i]}");
+            } 
+            Console.WriteLine(";");
+        }
+    }
+}
---
+++ Algorithms/Lesson_8/Sorter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Lesson_8
+{
+    public static class Sorter
+    {
+        public static int[] Bucketsort(int[] array, int blockCount = 5)
+        {
+            if (array.Length <= blockCount - 1) return Quicksort(array, 0, array.Length - 1);
+
+            int maxValue = 0; // Поиск максимального значения массива.
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] > maxValue) maxValue = array[i];
+
+            List<int> splitValues = new List<int>(); // Создание коллекции разделительных значений, для деления на блоки.
+            for (int i = 0; i < blockCount - 1; i++) splitValues.Add(maxValue / blockCount * (i + 1));
+            splitValues.Add(maxValue);
+
+            List<List<int>> blocks = new List<List<int>>(blockCount);
+            for (int i = 0; i < blockCount; i++) blocks.Add(new List<int>());
+
+            for (int i = 0; i < array.Length; i++) // Разбиение данных из массива на блоки.
+                for (int j = 0; j < blocks.Count; j++)
+                    if (array[i] <= splitValues[j])
+                    {
+                        blocks[j].Add(array[i]);
+                        break;
+                    }
+
+            for (int i = 0; i < blocks.Count; i++) // Сортировка каждого блока.
+                blocks[i] = new List<int>(Quicksort(blocks[i].ToArray(), 0, blocks[i].Count - 1));
+
+            for (int i = 0, j = 0; i < blocks.Count; i++) // Запись данных обратно в массив.
+                for (int l = 0; l < blocks[i].Count; l++, j++) array[j] = blocks[i][l];
+
+            return array;
+        }
+        public static int[] Quicksort(int[] array, int first, int last)
+        {
+            int i = first, j = last, x = array[(first + last) / 2];
+            do
+            {
+                while (array[i] < x) i++;
+                while (array[j] > x) j--;
+
+                if (i <= j)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (i < last) Quicksort(array, i, last);
+            if (first < j) Quicksort(array, first, j);
+            return array;
+        }
+    }
+}
